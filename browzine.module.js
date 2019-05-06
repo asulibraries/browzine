b3a6cfec0b0a74b706aeee92eba0047e90043047
Eli Zoller <zoller.eli@gmail.com>
2019-05-06 11:32:21 -0700

    trying to get the controller to pass in correctly

--- browzine.module.js
+++ browzine.module.js
@@ -1,10 +1,10 @@
 angular.module('browzineMod', [])
-  .controller('browzineController', [function ($scope) {
+  .controller('browzineController', ['$scope', function ($scope) {
     console.log("we are in the browzine module")
     var self = this;
     console.log(this);
     console.log(self.prmSearchResultAvailabilityLine)
-    $scope.ctrl = self.prmSearchResultAvailabilityLine;
+    $scope.ctrl = {'parentCtrl': self.prmSearchResultAvailabilityLine};
     window.browzine.primo.searchResult($scope);
   }])
   .component('browzineMod', {
