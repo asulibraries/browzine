31faf00c2d60e84f27316759e4a613814fcac74d
Eli Zoller <zoller.eli@gmail.com>
2019-05-06 11:39:09 -0700

    still debugging

--- browzine.module.js
+++ browzine.module.js
@@ -1,15 +1,37 @@
+// angular.module('browzineMod', [])
+//   .controller('browzineController', ['$scope', function ($scope) {
+//     console.log("we are in the browzine module")
+//     var self = this;
+//     console.log(this);
+//     console.log(self.prmSearchResultAvailabilityLine)
+//     $scope.ctrl = {'parentCtrl': self.prmSearchResultAvailabilityLine};
+//     window.browzine.primo.searchResult($scope);
+//   }])
+//   .component('browzineMod', {
+//     require: {
+//       prmSearchResultAvailabilityLine: '^prmSearchResultAvailabilityLine'
+//     },
+//     controller: 'browzineController'
+//   });
+
+
 angular.module('browzineMod', [])
-  .controller('browzineController', ['$scope', function ($scope) {
-    console.log("we are in the browzine module")
+  .controller('browzineController', [ function () {
     var self = this;
-    console.log(this);
-    console.log(self.prmSearchResultAvailabilityLine)
-    $scope.ctrl = {'parentCtrl': self.prmSearchResultAvailabilityLine};
-    window.browzine.primo.searchResult($scope);
+    console.log(self);
+
+    self.$onInit = function () {
+      console.log("in init")
+    }
+
   }])
   .component('browzineMod', {
     require: {
       prmSearchResultAvailabilityLine: '^prmSearchResultAvailabilityLine'
     },
-    controller: 'browzineController'
+    bindings: {
+      hideOnline: '<',
+      msg: '@?'
+    },
+    controller: 'browzineController',
   });
