8ef4d59b92e9bbeb5b999dddac74351f89a6e9a5
Eli Zoller <zoller.eli@gmail.com>
2019-06-06 16:50:30 -0700

    why no browzineWebLink

--- browzine.module.js
+++ browzine.module.js
@@ -53,7 +53,8 @@ angular.module('browzineMod', [])
         // console.log(URL);
         $http.get(URL).then(function(response) {
           self.data = response.data;
-          if (self.data && self.data.browzineWebLink) {
+          console.log(self.data);
+          if (self.data != {} && self.data.browzineWebLink) {
             self.browzineWebLink = self.data.browzineWebLink;
           }
           // console.log(vm.data);
@@ -168,7 +169,7 @@ angular.module('browzineMod', [])
         <md-icon md-svg-icon='primo-ui:open-in-new' class='md-primoExplore-theme' aria-hidden='true' style='height: 15px; width: 15px; min-height: 15px; min-width: 15px; margin-top: -2px;color: #757575;'><svg width='100%' height='100%' viewBox='0 0 24 24' y='504' xmlns='http://www.w3.org/2000/svg' fit='' preserveAspectRatio='xMidYMid meet' focusable='false'><path d='M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z'></path></svg></md-icon>\
         </a>\
       </div >\
-      <div class='browzine' style='line-height: 1.4em;' ng-if='$ctrl.browzineEnabled && ($ctrl.isArticle() || $ctrl.isJournal()) && $ctrl.data != {}'>\
+      <div class='browzine' style='line-height: 1.4em;' ng-if='$ctrl.browzineEnabled && (($ctrl.isArticle() && $ctrl.prmSearchResultAvailabilityLine.isFullView) || $ctrl.isJournal()) && $ctrl.data != {}'>\
       <a class='browzine-web-link' href='{{$ctrl.browzineWebLink}}' target='_blank'>\
           <img src='{{$ctrl.bookIcon}}' class='browzine-book-icon' style='margin-bottom: -2px; margin-right: 2.5px;' aria-hidden='true' width='15' height='15'/>\
           <span class='browzine-web-link-text'>{{$ctrl.browzineWebLinkText}}</span>\
