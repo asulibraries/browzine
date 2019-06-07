ceb43e01d4efdaf30d4f2784a0d9f423bc2d2183
Eli Zoller <zoller.eli@gmail.com>
2019-06-07 06:34:39 -0700

    refactor attempt

--- browzine.module.js
+++ browzine.module.js
@@ -20,7 +20,7 @@ angular.module('browzineMod', [])
       self.articlePDFDownloadLinkText = "View PDF";
       self.printRecordsIntegrationEnabled = true;
       self.browzineWebLinkText = "";
-      self.data = {};
+      self.data = null;
       self.pdfIcon = "https://assets.thirdiron.com/images/integrations/browzine-pdf-download-icon.svg";
       self.bookIcon = "https://assets.thirdiron.com/images/integrations/browzine-open-book-icon.svg";
       self.apiKey = "a1d2656d-d27c-466f-b549-f14a645a2024";
@@ -30,39 +30,30 @@ angular.module('browzineMod', [])
       // console.log(self.browzineEnabled);
       self.result = self.getResult();
       self.browzineWebLink = null;
-
-      if(self.browzineEnabled && self.result){
-        // console.log("we're enabled and have a result");
-        self.getData();
-        console.log(self.data);
+      self.isJournal = null;
+      self.isArticle = null;
+      self.response = null;
+      self.journal = null;
+      self.doi = "";
+      self.issn = "";
+      if(self.browzineEnabled && self.result && (self.isArticle() || self.isJournal())){
+        console.log("we're enabled and have a result");
+        // self.getData();
+        // console.log(self.data);
+        self.endpoint = self.getEndpoint();
+        if (self.endpoint){
+          $http.get(self.endpoint).then(function(response){
+            console.log("we got the data from browzine");
+            console.log(response.data);
+            self.data = response.data;
+            self.response = response;
+            console.log(self.response);
+          }, function(error){
+            console.log(error);
+          });
+        }
       }
     }
-
-    self.getData = function() {
-      console.log("in getData");
-      var URL = "";
-
-      if (self.isJournal()) {
-        URL = self.api + "/search?issns=" + self.issn();
-      }
-      if (self.isArticle()) {
-        URL = self.api + "/articles/doi/" + self.doi() + "?include=journal";
-      }
-      if (URL){
-        URL += "&access_token=" + self.apiKey;
-        // console.log(URL);
-        $http.get(URL).then(function(response) {
-          self.data = response.data;
-          console.log(self.data);
-          if (self.data != {} && self.data.browzineWebLink) {
-            self.browzineWebLink = self.data.browzineWebLink;
-          }
-          // console.log(vm.data);
-        }, function (error) {
-          console.log(error);
-        });
-      }
-    };
 
     self.isArticle = function() {
       var validation = false;
@@ -77,9 +68,7 @@ angular.module('browzineMod', [])
           }
         }
       }
-      if (validation){
-        self.browzineWebLinkText = self.articleBrowZineWebLinkText;
-      }
+      self.isArticle = validation;
       return validation;
     };
 
@@ -96,9 +85,7 @@ angular.module('browzineMod', [])
           }
         }
       }
-      if (validation) {
-        self.browzineWebLinkText = self.journalBrowZineWebLinkText;
-      }
+      self.isJournal = validation;
       return validation;
     }
 
@@ -106,28 +93,7 @@ angular.module('browzineMod', [])
       return self.prmSearchResultAvailabilityLine.result || self.prmSearchResultAvailabilityLine.item;
     };
 
-    self.directToPDFUrl = function() {
-      if (self.data.fullTextFile) {
-        return self.data.fullTextFile;
-      }
-      return null;
-    };
-
-    self.doi = function(){
-      var doi = "";
-      var result = self.result;
-      if (result && result.pnx) {
-        if (result.pnx.addata && result.pnx.addata.doi) {
-          if (result.pnx.addata.doi[0]) {
-            doi = result.pnx.addata.doi[0].trim();
-          }
-        }
-      }
-
-      return encodeURIComponent(doi);
-    };
-
-    self.issn = function(){
+    self.getIssn = function(){
       var issn = "";
       var result = self.result;
 
@@ -152,8 +118,164 @@ angular.module('browzineMod', [])
           }
         }
       }
-
+      self.issn = encodeURIComponent(issn);
       return encodeURIComponent(issn);
+    }
+
+    self.getDoi = function(){
+      var doi = "";
+      var result = self.result;
+
+      if (result && result.pnx) {
+        if (result.pnx.addata && result.pnx.addata.doi) {
+          if (result.pnx.addata.doi[0]) {
+            doi = result.pnx.addata.doi[0].trim();
+          }
+        }
+      }
+      self.doi = encodeURIComponent(doi);
+      return encodeURIComponent(doi);;
+    }
+
+    self.getEndpoint = function(){
+      var endpoint = "";
+
+      if (self.isArticle) {
+        var doi = self.doi;
+        endpoint = api + "/articles/doi/" + doi + "?include=journal";
+      }
+
+      if (self.isJournal) {
+        var issn = self.issn;
+        endpoint = api + "/search?issns=" + issn;
+      }
+
+      endpoint += "&access_token=" + apiKey;
+
+      return endpoint;
+    }
+
+    self.getData = function(){
+      var data = {};
+      var response = self.response;
+
+      if (Array.isArray(response.data)) {
+        data = response.data.filter(function (journal) {
+          return journal.browzineEnabled === true;
+        }).pop();
+      } else {
+        data = response.data;
+      }
+
+      return data;
+    }
+
+    self.getIncludedJournal = function(){
+      var response = self.response;
+      var journal = null;
+
+      if (response.included) {
+        journal = Array.isArray(response.included) ? response.included[0] : response.included;
+      }
+      self.journal = journal;
+      return journal;
+    }
+
+    self.getBrowzineWebLink = function(){
+      var data = self.data;
+      var browzineWebLink = null;
+
+      if (data && data.browzineWebLink) {
+        browzineWebLink = data.browzineWebLink;
+      }
+      self.browzineWebLink = browzineWebLink;
+      return browzineWebLink;
+    }
+
+    self.getCoverImage = function(){
+      var data = self.data;
+      var coverImageUrl = null;
+      var journal = self.journal;
+      if(!journal){
+        journal = self.getIncludedJournal();
+      }
+
+      if (self.isJournal) {
+        if (data && data.coverImageUrl) {
+          coverImageUrl = data.coverImageUrl;
+        }
+      }
+
+      if (self.isArticle) {
+        if (journal && journal.coverImageUrl) {
+          coverImageUrl = journal.coverImageUrl;
+        }
+      }
+
+      return coverImageUrl;
+    }
+
+    self.getBrowzineEnabled = function(){
+      var browzineEnabled = false;
+      var data = self.data;
+      var journal = self.journal;
+      if (!journal) {
+        journal = self.getIncludedJournal();
+      }
+
+      if (self.isJournal) {
+        if (data && data.browzineEnabled) {
+          browzineEnabled = data.browzineEnabled;
+        }
+      }
+
+      if (self.isArticle) {
+        if (journal && journal.browzineEnabled) {
+          browzineEnabled = journal.browzineEnabled;
+        }
+      }
+
+      return browzineEnabled;
+    }
+
+    self.isDefaultCoverImage = function(coverImageUrl) {
+      var defaultCoverImage = false;
+
+      if (coverImageUrl && coverImageUrl.toLowerCase().indexOf("default") > -1) {
+        defaultCoverImage = true;
+      }
+
+      return defaultCoverImage;
+    }
+
+    self.getDirectToPDFUrl = function(){
+      var directToPDFUrl = null;
+      var data = self.data;
+
+      if (self.isArticle) {
+        if (data.fullTextFile) {
+          directToPDFUrl = data.fullTextFile;
+        }
+      }
+
+      return directToPDFUrl;
+    }
+
+    self.isFiltered = function() {
+      var validation = false;
+      var result = self.result;
+
+      if (result && result.delivery) {
+        if (result.delivery.deliveryCategory) {
+          var deliveryCategory = result.delivery.deliveryCategory[0].trim().toLowerCase();
+
+          if (deliveryCategory === "alma-p" && !showPrintRecords()) {
+            validation = true;
+          }
+        }
+      }
+
+      return validation;
     };
 
   }])
@@ -162,14 +284,14 @@ angular.module('browzineMod', [])
       prmSearchResultAvailabilityLine: '^prmSearchResultAvailabilityLine'
     },
     controller: 'browzineController',
-    template: "<div class='browzine' style='line-height: 1.4em; margin-right: 4.5em;'           ng-if='$ctrl.directToPDFUrl() != null && $ctrl.isArticle() &&              $ctrl.articlePDFDownloadLinkEnabled && $ctrl.browzineEnabled' >\
+    template: "<div class='browzine' style='line-height: 1.4em; margin-right: 4.5em;'           ng-if='$ctrl.getDirectToPDFUrl() && $ctrl.isArticle &&              $ctrl.articlePDFDownloadLinkEnabled && $ctrl.getBrowzineEnabled()' >\
         <a class='browzine-direct-to-pdf-link' href='{{$ctrl.directToPDFUrl()}}' target='_blank'>\
         <img src='{{$ctrl.pdfIcon}}' class='browzine-pdf-icon' style='margin-bottom: -3px; margin-right: 2.8px;' aria-hidden='true' width='12' height='16' />\
         <span class='browzine-web-link-text'>{{ $ctrl.articlePDFDownloadLinkText }}</span>\
         <md-icon md-svg-icon='primo-ui:open-in-new' class='md-primoExplore-theme' aria-hidden='true' style='height: 15px; width: 15px; min-height: 15px; min-width: 15px; margin-top: -2px;color: #757575;'><svg width='100%' height='100%' viewBox='0 0 24 24' y='504' xmlns='http://www.w3.org/2000/svg' fit='' preserveAspectRatio='xMidYMid meet' focusable='false'><path d='M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z'></path></svg></md-icon>\
         </a>\
       </div >\
-      <div class='browzine' style='line-height: 1.4em;' ng-if='$ctrl.browzineEnabled && (($ctrl.isArticle() && $ctrl.prmSearchResultAvailabilityLine.isFullView) || $ctrl.isJournal()) && $ctrl.data != {}'>\
+      <div class='browzine' style='line-height: 1.4em;' ng-if='$ctrl.browzineEnabled && (($ctrl.isArticle && $ctrl.prmSearchResultAvailabilityLine.isFullView) || $ctrl.isJournal) && $ctrl.getBrowzineWebLink() != null'>\
       <a class='browzine-web-link' href='{{$ctrl.browzineWebLink}}' target='_blank'>\
           <img src='{{$ctrl.bookIcon}}' class='browzine-book-icon' style='margin-bottom: -2px; margin-right: 2.5px;' aria-hidden='true' width='15' height='15'/>\
           <span class='browzine-web-link-text'>{{$ctrl.browzineWebLinkText}}</span>\
