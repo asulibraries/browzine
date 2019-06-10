a4f8f8157f22cbc86f4fe21119a50d35241fbaf1
Eli Zoller <zoller.eli@gmail.com>
2019-06-10 13:44:38 -0700

    can't use  in the template so we'll assign it to a variable on the  instead

--- browzine.module.js
+++ browzine.module.js
@@ -31,7 +31,6 @@ angular.module('browzineMod', [])
           self.getFromEndpoint(self.endpoint);
         }
       }
-      console.log("isFullView" + self.prmSearchResultAvailabilityLine.isFullView);
     }
 
     self.getFromEndpoint = function(){
@@ -44,7 +43,8 @@ angular.module('browzineMod', [])
         self.browzineEnabled = self.getBrowzineEnabled();
         self.browzineWebLink = self.getBrowzineWebLink();
         self.directToPDFUrl = self.getDirectToPDFUrl();
-        console.log(self.isArticleTF && $window.browzine.articlePDFDownloadLinkEnabled && self.browzineEnabled && self.directToPDFUrl != null);
+        self.articlePDFDownloadLinkEnabled = $window.browzine.articlePDFDownloadLinkEnabled;
+        self.articlePDFDownloadLinkText = $window.browzine.articlePDFDownloadLinkText;
       }, function (error) {
         console.log(error);
       });
@@ -250,10 +250,10 @@ angular.module('browzineMod', [])
       prmSearchResultAvailabilityLine: '^prmSearchResultAvailabilityLine'
     },
     controller: 'browzineController',
-    template: "<div class='browzine' style='line-height: 1.4em; margin-right: 4.5em;'           ng-if='$ctrl.isArticleTF && $ctrl.browzineEnabled && $ctrl.directToPDFUrl != null' >\
+    template: "<div class='browzine' style='line-height: 1.4em; margin-right: 4.5em;'           ng-if='$ctrl.isArticleTF && $ctrl.browzineEnabled && $ctrl.articlePDFDownloadLinkEnabled && $ctrl.directToPDFUrl != null' >\
         <a class='browzine-direct-to-pdf-link' href='{{$ctrl.directToPDFUrl}}' target='_blank'>\
         <img src='https://assets.thirdiron.com/images/integrations/browzine-pdf-download-icon.svg' class='browzine-pdf-icon' style='margin-bottom: -3px; margin-right: 2.8px;' aria-hidden='true' width='12' height='16' />\
-        <span class='browzine-web-link-text'>{{ $window.browzine.articlePDFDownloadLinkText }}</span>\
+        <span class='browzine-web-link-text'>{{ $ctrl.articlePDFDownloadLinkText }}</span>\
         <md-icon md-svg-icon='primo-ui:open-in-new' class='md-primoExplore-theme' aria-hidden='true' style='height: 15px; width: 15px; min-height: 15px; min-width: 15px; margin-top: -2px;color: #757575;'><svg width='100%' height='100%' viewBox='0 0 24 24' y='504' xmlns='http://www.w3.org/2000/svg' fit='' preserveAspectRatio='xMidYMid meet' focusable='false'><path d='M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z'></path></svg></md-icon>\
         </a>\
       </div >\
